973f057507ba4343de73f3588a0992392f451c6c
CRUXx11 <56359161+CRUXx11@users.noreply.github.com>
2021-03-30 21:08:29 +0530

    already added in web development folder

--- Javascript Logical Assignment/Basic/Q5/Q5.js
+++
@@ -1,9 +0,0 @@
-var val = document.getElementById('bu');
-val.addEventListener('click', function padChars(e) {
-    e.preventDefault();
-    var x = document.getElementById('num1').value;
-    var y = document.getElementById('text1').value;
-    for(let i=0;i<x;i++){
-        document.getElementById('result').innerHTML =document.getElementById('result').innerHTML+y;           
-    }
-})
